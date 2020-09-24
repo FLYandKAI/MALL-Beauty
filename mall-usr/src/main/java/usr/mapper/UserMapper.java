8633e10383f45096d08cc6bb8d09272af071d528
LangFo <583378942@qq.com>
2020-09-24 16:12:16 +0800

    初始模块--第一版

---
+++ mall-usr/src/main/java/usr/mapper/UserMapper.java
@@ -0,0 +1,16 @@
+package usr.mapper;
+
+import usr.entites.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表 Mapper 接口
+ * </p>
+ *
+ * @author 郑树凯
+ * @since 2020-09-20
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
---
+++ mall-usr/src/main/java/usr/service/impl/UserServiceImpl.java
@@ -0,0 +1,20 @@
+package usr.service.impl;
+
+import usr.entites.User;
+import usr.mapper.UserMapper;
+import usr.service.UserService;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
+
+/**
+ * <p>
+ * 用户表 服务实现类
+ * </p>
+ *
+ * @author 郑树凯
+ * @since 2020-09-20
+ */
+@Service
+public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements UserService {
+
+}
